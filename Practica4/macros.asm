75625fb6b9c1c91a06f7ad1d4344414919925dae
DominicRuano <Dojuparupe1975@gmail.com>
2024-04-14 20:26:22 -0600

    Main menu added se agrego el menu inicial

--- Practica4/macros.asm
+++ Practica4/macros.asm
@@ -1,3 +1,38 @@
+
+ImprimirMenuInicial MACRO 
+    LimpiarConsola
+    ImprimirCadenas salto
+
+    ImprimirCadenasColor MSGMenu1, colorRojoTexto
+    ImprimirCadenasColor MSGMenu2, colorRojoTexto
+    ImprimirCadenasColor MSGMenu3, colorRojoTexto
+    ImprimirCadenasColor MSGMenu4, colorRojoTexto
+
+    ImprimirCadenas salto
+
+    ImprimirCadenasColor MSG1OP1, colorCianTexto
+    ImprimirCadenasColor MSG1OP2, colorCianTexto
+    ImprimirCadenasColor MSG1OP3, colorCianTexto
+
+    ImprimirCadenas salto
+
+    ImprimirCadenasColor MSG2OP1, colorCianTexto
+    ImprimirCadenasColor MSG2OP2, colorCianTexto
+    ImprimirCadenasColor MSG2OP3, colorCianTexto
+
+    ImprimirCadenas salto
+
+    ImprimirCadenasColor MSG3OP1, colorCianTexto
+    ImprimirCadenasColor MSG3OP2, colorCianTexto
+    ImprimirCadenasColor MSG3OP3, colorCianTexto
+
+    ImprimirCadenas salto
+
+    ImprimirCadenasColor MSG4OP1, colorCianTexto
+    ImprimirCadenasColor MSG4OP2, colorCianTexto
+    ImprimirCadenasColor MSG4OP3, colorCianTexto
+ENDM
+
 obtenerOpcion MACRO regOpcion
     MOV AH, 01h
     INT 21h
@@ -41,4 +76,27 @@ ImprimirCadenas MACRO registroPrint
     MOV AH, 09h
     LEA DX, registroPrint
     INT 21h
+ENDM
+
+ImprimirCadenasColor MACRO registroPrint, color
+    PUSH AX         ; Guardar el registro AX
+    PUSH BX         ; Guardar el registro BX
+    PUSH CX         ; Guardar el registro CX
+
+    ; Establecer el color de la letra para la pantalla
+    MOV AH, 09h     ; Función de BIOS para escribir carácter y atributo
+    MOV AL, ' '     ; Carácter a escribir (espacio en este caso)
+    MOV BH, 0       ; Página de video (usualmente 0)
+    MOV BL, color   ; Color del texto (4 bits de fondo, 4 bits de texto)
+    MOV CX, lengthof registroPrint       ; Número de veces que se escribe el carácter
+    INT 10h         ; Interrupción de video
+
+    ; Imprimir la cadena en pantalla
+    MOV AH, 09h     ; Función de DOS para imprimir cadena
+    LEA DX, registroPrint ; Carga la dirección de la cadena en DX
+    INT 21h         ; Interrupción de DOS para imprimir la cadena
+
+    POP CX          ; Restaurar el registro CX
+    POP BX          ; Restaurar el registro BX
+    POP AX          ; Restaurar el registro AX
 ENDM
--- Practica4/main.asm
+++ Practica4/main.asm
@@ -3,18 +3,87 @@ INCLUDE macros.ASM
 .MODEL small
 .STACK 64h
 .DATA
-    msg db " hola mundo", "$"
-    
+salto db 10,13, "$"
+Prueba db 10 ,13, "Hola mundo", 10 ,13, "$"
+opcion db 1 dup(?)
+
+colorNegroTexto db 00h ; Negro sobre negro
+colorAzulTexto db 01h ; Azul sobre negro
+colorVerdeTexto db 02h ; Verde sobre negro
+colorCianTexto db 03h ; Cian sobre negro
+colorRojoTexto db 04h ; Rojo sobre negro
+colorMagentaTexto db 05h ; Magenta sobre negro
+colorMarronTexto db 06h ; Marrón sobre negro
+colorGrisClaroTexto db 07h ; Gris claro sobre negro
+colorGrisOscuroTexto db 08h ; Gris oscuro sobre negro
+colorAzulClaroTexto db 09h ; Azul claro sobre negro
+colorVerdeClaroTexto db 0Ah ; Verde claro sobre negro
+colorCianClaroTexto db 0Bh ; Cian claro sobre negro
+colorRojoClaroTexto db 0Ch ; Rojo claro sobre negro
+colorMagentaClaroTexto db 0Dh ; Magenta claro sobre negro
+colorAmarilloTexto db 0Eh ; Amarillo sobre negro
+colorBlancoTexto db 0Fh ; Blanco sobre negro
+
+MSGMenu1 db "  ____  ____  ____  ____  _________  ____  ____  ____  ____  ____  ____  ____ ", 10 ,13, "$"
+MSGMenu2 db " ||M ||||e ||||n ||||u ||||       ||||I ||||n ||||i ||||c ||||i ||||a ||||l ||", 10 ,13, "$"
+MSGMenu3 db " ||__||||__||||__||||__||||_______||||__||||__||||__||||__||||__||||__||||__||", 10 ,13, "$"
+MSGMenu4 db " |/__\||/__\||/__\||/__\||/_______\||/__\||/__\||/__\||/__\||/__\||/__\||/__\|", 10 ,13, "$"
+
+MSG1OP1 db "                    ___       __                ___  __   __ ", 10 ,13, "$"
+MSG1OP2 db "    |    |\ | |  | |__  \  / /  \       | |  | |__  / _` /  \", 10 ,13, "$"
+MSG1OP3 db "    |.   | \| \__/ |___  \/  \__/    \__/ \__/ |___ \__> \__/", 10 ,13, "$"
+
+MSG2OP1 db "  __                             __     __      ", 10 ,13, "$"
+MSG2OP2 db "  __|     /\  |\ | |  |\/|  /\  /  ` | /  \ |\ |", 10 ,13, "$"
+MSG2OP3 db " |__ .   /~~\ | \| |  |  | /~~\ \__, | \__/ | \|", 10 ,13, "$"
+
+MSG3OP1 db "  __             ___  __   __              __     __      ", 10 ,13, "$"
+MSG3OP2 db "  __|    | |\ | |__  /  \ |__)  |\/|  /\  /  ` | /  \ |\ |", 10 ,13, "$"
+MSG3OP3 db "  __|.   | | \| |    \__/ |  \  |  | /~~\ \__, | \__/ | \|", 10 ,13, "$"
+
+MSG4OP1 db "           __               __  ", 10 ,13, "$"
+MSG4OP2 db "  |__|    /__`  /\  |    | |__) ", 10 ,13, "$"
+MSG4OP3 db "     |.   .__/ /~~\ |___ | |  \ ", 10 ,13, "$"
+
+IngreseOpcion db "Ingrese una opcion: ", "$"
 .CODE
     MOV AX, @data
     MOV DS, AX
 
     ; Metodo Principal (Main)
     Main PROC
+        Menu:
+            ImprimirMenuInicial
+
+            ImprimirCadenas IngreseOpcion
+            obtenerOpcion opcion
+
+            CMP opcion, "1"
+            JE Opcion1
+
+            CMP opcion, "2"
+            JE Opcion2
+
+            CMP opcion, "3"
+            JE Opcion3
+
+            CMP opcion, "4"
+            JE Salir
+
+            JMP Menu
+
+        Opcion1:
+            JMP Menu
+
+        Opcion2:
+            JMP Menu
+        
+        Opcion3:
+            JMP Menu
+
 
         Salir:
-            ImprimirCadenas msg
-            
+            ImprimirCadenas salto
             MOV AX, 4C00h ; Interrupcion Para Terminar Programa
             INT 21h
     Main ENDP
