ad8e8df0bb6302ee08e472d685c59bdd4e9489d3
DominicRuano <Dojuparupe1975@gmail.com>
2024-04-20 23:07:26 -0600

    endgame

--- Practica4/main.asm
+++ Practica4/main.asm
@@ -1,4 +1,5 @@
 INCLUDE macros2.ASM
+INCLUDE macros.ASM
 
 .MODEL small
 .STACK 64h
@@ -43,6 +44,50 @@ segundos db 2 dup("$")
 Barra db "|", "$"
 horaDB db 18 dup("$")
 nombreDB db "DB.txt", 0
+
+
+    handler             dw ?
+    filename            db "Input2.txt", 0
+    errorCode           db ?
+    errorOpenFile       db "Ocurrio Un Error Al Abrir El Archivo - ERRCODE: ", "$"
+    errorCloseFile      db "Ocurrio Un Error Al Cerrar El Archivo - ERRCODE: ", "$"
+    errorReadFile       db "Ocurrio Un Error Al Leer El Archivo - ERRCODE: ", "$"
+    errorSizeFile       db "Ocurrio Un Error Obteniendo El Size Del Archivo - ERRCODE: ", "$"
+    exitOpenFileMsg     db "El Archivo Se Abrio Correctamente", "$"
+    exitCloseFileMsg    db "El Archivo Se Cerro Correctamente", "$"
+    exitSizeFileMsg     db "Se Obtuvo La Longitud Correctamente", "$"
+    bufferTemporal      db 80 dup("$")
+    bufferImagen1       db 900 dup("$")
+    bufferImagen2       db 900 dup("$")
+    bufferImagen3       db 900 dup("$")
+    bufferImagen4       db 900 dup("$")
+    bufferImagen5       db 900 dup("$")
+    bufferImagen6       db 900 dup("$")
+    bufferImagen7       db 900 dup("$")
+    bufferImagen8       db 900 dup("$")
+    charsPerRow1        dw ?
+    cantRows1           dw ?
+    charsPerRow2        dw ?
+    cantRows2           dw ?
+    charsPerRow3        dw ?
+    cantRows3           dw ?
+    charsPerRow4        dw ?
+    cantRows4           dw ?
+    charsPerRow5        dw ?
+    cantRows5           dw ?
+    charsPerRow6        dw ?
+    cantRows6           dw ?
+    charsPerRow7        dw ?
+    cantRows7           dw ?
+    charsPerRow8        dw ?
+    cantRows8           dw ?
+    saltoCadena         dw ?
+    extensionArchivo    dw 0
+    posApuntador        dw 0
+    filaActual          db 0
+    fila                db 0
+    columna             db 5
+    paginaActual        db 0
 
 
 colorNegroTexto db 00h ; Negro sobre negro
@@ -395,9 +440,104 @@ String4 db 00h, 00h, 00h, 00h, 00h
                     ImprimirReporte
                     JMP NuevoJuevo
 
-            Animacion:
+            Animacion:  
                 LimpiarConsola
-                PresioneTeclaParaContinuar
+                CargarFile
+
+
+                MOV AH, 10h
+                INT 16h
+
+                MOV AX, 03h
+                INT 10h
+
+            CicloAnimaciones:
+                MOV fila, 0
+                MOV filaActual, 0
+                MOV columna, 0
+                
+                CMP paginaActual, 0 ; * Salto A Pagina 1
+                JZ EtAnimacion1Aux
+
+                CMP paginaActual, 1 ; * Salto A Pagina 2
+                JZ EtAnimacion2Aux
+
+                CMP paginaActual, 2 ; * Salto A Pagina 3
+                JZ EtAnimacion3Aux
+
+                CMP paginaActual, 3 ; * Salto A Pagina 4
+                JZ EtAnimacion4Aux
+
+                CMP paginaActual, 4 ; * Salto A Pagina 5
+                JZ EtAnimacion5Aux
+
+                CMP paginaActual, 5; * Salto A Pagina 5
+                JZ EtAnimacion6Aux
+
+                CMP paginaActual, 6 ; * Salto A Pagina 5
+                JZ EtAnimacion7Aux
+
+                CMP paginaActual, 7 ; * Salto A Pagina 5
+                JZ EtAnimacion8Aux
+
+                JMP Salir   ; ! -> Si "paginaActual" Sobrepasa A Los Valores Definidos Que Salga Del Ciclo
+
+                ; ? ETIQUETAS AUXILIARES PARA EVITAR ERRORES CON LA LONGITUD DE SALTOS
+                EtAnimacion1Aux:    
+                    JMP EtAnimacion1
+                EtAnimacion2Aux:
+                    JMP EtAnimacion2
+                EtAnimacion3Aux:
+                    JMP EtAnimacion3
+                EtAnimacion4Aux:
+                    JMP EtAnimacion4
+                EtAnimacion5Aux:
+                    JMP EtAnimacion5
+                EtAnimacion6Aux:
+                    JMP EtAnimacion6
+                EtAnimacion7Aux:
+                    JMP EtAnimacion7
+                EtAnimacion8Aux:
+                    JMP EtAnimacion8
+
+                ; * Invocacion Animacion 1
+                EtAnimacion1:
+                    Animacion1 charsPerRow1, cantRows1, bufferImagen1
+                    JMP CicloAnimaciones
+
+                ; * Invocacion Animacion 2
+                EtAnimacion2:
+                    Animacion2 charsPerRow2, cantRows2, bufferImagen2
+                    JMP CicloAnimaciones
+
+                ; * Invocacion Animacion 3
+                EtAnimacion3:
+                    Animacion3 charsPerRow3, cantRows3, bufferImagen3
+                    JMP CicloAnimaciones
+
+                ; * Invocacion Animacion 4
+                EtAnimacion4:
+                    Animacion4 charsPerRow4, cantRows4, bufferImagen4
+                    JMP CicloAnimaciones
+
+                EtAnimacion5:
+                    Animacion1 charsPerRow5, cantRows5, bufferImagen5
+                    JMP CicloAnimaciones
+
+                    EtAnimacion6:
+                    Animacion1 charsPerRow6, cantRows6, bufferImagen6
+                    JMP CicloAnimaciones
+
+                    EtAnimacion7:
+                    Animacion1 charsPerRow7, cantRows7, bufferImagen7
+                    JMP CicloAnimaciones
+                    
+                    EtAnimacion8:
+                    Animacion1 charsPerRow8, cantRows8, bufferImagen8
+                    JMP CicloAnimaciones
+
+
+
                 JMP Menu
             
             Informacion:
